f2c2acbba87a6a3bce5df7f4ab6534a81945a9f6
Liliia <lili4kadrebot@gmail.com>
2021-10-16 21:16:48 +0300

    test

---
+++ #src/js/script.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+
+$(document).ready(function() {
+    console.log('js is already done')
+})
---
+++ gulp_template/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _jquery = _interopRequireDefault(require("jquery"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+(0, _jquery["default"])(document).ready(function () {
+  console.log('js is already done');
+});
